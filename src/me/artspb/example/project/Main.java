b49a836eeb3394be451c10d12f5a2d792a64440f
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2018-06-27 23:01:21 +0200

    [bad] introduce method

--- src/me/artspb/example/project/Main.java
+++ src/me/artspb/example/project/Main.java
@@ -3,7 +3,11 @@ package me.artspb.example.project;
 public class Main {
 
     public static void main(String[] args) {
-        sayHello(String.format("%s, %s!", "Hello", "world"));
+        sayHello(format());
+    }
+
+    private static String format() {
+        return String.format("%s, %s!", "Hello", "world");
     }
 
     private static void sayHello(String message) {
