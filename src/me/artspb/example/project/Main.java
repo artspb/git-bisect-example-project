ddd097e9a2968c5704d90b802eabfda02706607d
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2018-06-27 23:00:58 +0200

    [bad] format string

--- src/me/artspb/example/project/Main.java
+++ src/me/artspb/example/project/Main.java
@@ -3,7 +3,7 @@ package me.artspb.example.project;
 public class Main {
 
     public static void main(String[] args) {
-        sayHello("Hello, world!");
+        sayHello(String.format("%s, %s!", "Hello", "world"));
     }
 
     private static void sayHello(String message) {
