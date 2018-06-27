6af2ed56512d793d3238370227637b2d202c2dd6
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2018-06-27 23:00:12 +0200

    [bad] broken test

--- test/me/artspb/example/project/MainTest.java
+++ test/me/artspb/example/project/MainTest.java
@@ -2,9 +2,12 @@ package me.artspb.example.project;
 
 import org.junit.Test;
 
+import static org.junit.Assert.fail;
+
 public class MainTest {
 
     @Test
     public void main() {
+        fail();
     }
 }
