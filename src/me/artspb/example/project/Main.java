57413362ff22f70ef81298ea3dc0603ee52d450e
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2018-06-27 22:59:04 +0200

    [good] working on code

--- src/me/artspb/example/project/Main.java
+++ src/me/artspb/example/project/Main.java
@@ -2,6 +2,10 @@ package me.artspb.example.project;
 
 public class Main {
     public static void main(String[] args) {
+        sayHello();
+    }
+
+    private static void sayHello() {
         System.out.println("Hello, world!");
     }
 }
